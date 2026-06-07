ae9c0fcdefb1781ed71fb19429c7914ec55950d2
christian20192024 <crisstianyaselga10@gmail.com>
2026-06-07 12:53:43 -0500

    Descripción de los cambios

--- app/src/main/java/com/proyecto/devops/HealthController.java
+++ app/src/main/java/com/proyecto/devops/HealthController.java
@@ -1,34 +1,18 @@
 package com.proyecto.devops;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.Map;
-
 @RestController
 public class HealthController {
-
     private static final Logger log = LoggerFactory.getLogger(HealthController.class);
-
     @GetMapping("/")
     public Map<String, String> home() {
-        log.info("Home endpoint requested");
-
-        return Map.of(
-                "app", "Proyecto DevOps",
-                "status", "running"
-        );
+        return Map.of("app", "Proyecto DevOps", "status", "running");
     }
-
     @GetMapping("/api/ping")
     public Map<String, String> ping() {
-        log.info("Ping endpoint requested");
-
-        return Map.of(
-                "message", "pong",
-                "status", "ok"
-        );
+        throw new RuntimeException("Error 500 critico simulado para el post-mortem " + Math.random());
     }
 }
